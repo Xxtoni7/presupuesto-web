9cbecb25db1ddcf2803207bd46e097d0c952c941
Xxtoni7 <toniwpp8@gmail.com>
2026-05-14 17:22:28 -0300

    cambiar URL de la API a Render

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "https://localhost:7277";
+//export const API_BASE_URL = "https://localhost:7277";
+export const API_BASE_URL = "https://presupuestos-api-qjo2.onrender.com";
